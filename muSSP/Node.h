5c5bef08793f739aa24839b5ab4d13b969957f79
tomoya.kimura <tomoya.kimura@tier4.jp>
2022-03-23 17:54:07 +0900

    add copy constructor
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

--- muSSP/Node.h
+++ muSSP/Node.h
@@ -5,10 +5,9 @@ class Node
 {
 public:
     Node() = default;
-//    int get_id() const;
+    // copy constructor
+    Node(const Node & node);
 
-//    int node_id = 0;
-//    double shortest_path;
     std::vector<int> precursor_idx;
     std::vector<int> precursor_edges_idx;
     std::vector<double> precursor_edges_weights;
